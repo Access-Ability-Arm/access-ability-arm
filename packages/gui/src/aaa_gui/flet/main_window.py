6ee745960b3bd333d8f963d86ea5be61ad260b19
Chris Kennedy <chrisken@gmail.com>
2025-11-16 10:36:52 -0500

    Increase control panel to 750px and window to 820px to eliminate scroll bar

--- packages/gui/src/aaa_gui/flet/main_window.py
+++ packages/gui/src/aaa_gui/flet/main_window.py
@@ -454,7 +454,7 @@ class FletMainWindow:
                     content=auto_tab_content,
                 ),
             ],
-            height=730,  # Fixed height for tab content (includes tab bar + speed slider)
+            height=750,  # Fixed height for tab content (includes tab bar + speed slider)
         )
 
         return ft.Container(
